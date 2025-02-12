e66cf003cc78058c4933af251d994c8ce068d333
jangwoo0305 <tjwkddn970305@gmail.com>
2025-02-12 22:52:41 +0900

    groundcheck raycast position freeze

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,31 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed = 5f; // 최대 속도
     public float jumpForce = 8f; // 점프 힘
     public Transform groundCheck; // 바닥 체크용 오브젝트
     public LayerMask groundLayer; // 바닥 감지 레이어
 
-    private Rigidbody2D rigid;
     private SpriteRenderer spriteRenderer;
     private Animator anim;
     private BoxCollider2D boxCollider;
 
     private bool isGrounded = false;
     private Vector2 originalOffset; // 기본 offset 값 저장
+    private Vector2 originallocalPosition;
+    private Vector3 velocity; // 이동 속도 관리용 변수
 
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        rigid.freezeRotation = true; // 회전 방지
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
 
         originalOffset = boxCollider.offset;
-
+        originallocalPosition = groundCheck.localPosition;
+        velocity = Vector3.zero; // 초기 속도 설정
     }
 
     void Update()
@@ -38,60 +38,58 @@ public class PlayerController : MonoBehaviour
         {
             spriteRenderer.flipX = h < 0;  // 좌측 방향일 때 flipX 활성화
             FlipCollider(spriteRenderer.flipX);
-            anim.SetBool("isRunning", true); // 좌우 키 입력 시 달리기 애니메이션
+            anim.SetBool("isRunning", true); // 달리기 애니메이션
         }
         else
         {
-            anim.SetBool("isRunning", false); // 좌우 키 입력 없으면 달리기 애니메이션 끄기
+            anim.SetBool("isRunning", false);
         }
 
-        // 바닥 체크 (Raycast)
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
+        // 바닥 체크 (OverlapCircle)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-        // 🔍 isGrounded 값 확인하기
         Debug.Log("Is Grounded: " + isGrounded);
-
-        // Ray를 그려서 확인
-        Debug.DrawRay(groundCheck.position, Vector2.down * 0.5f, isGrounded ? Color.green : Color.red);
+        Debug.DrawRay(groundCheck.position, Vector2.down * 0.1f, isGrounded ? Color.green : Color.red);
 
         // 점프 입력 처리
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Debug.Log("Jumping!");  // 점프 입력 감지 확인
+            Debug.Log("Jumping!");  
             anim.SetBool("isJumping", true);
             anim.SetBool("isFalling", false);
-            rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, jumpForce);
+            velocity.y = jumpForce; // 점프 시 Y축 속도 추가
         }
 
-        // 낙하 애니메이션 처리
-        if (rigid.linearVelocity.y < 0 && !isGrounded)
+        // 중력 적용
+        if (!isGrounded)
+        {
+            velocity.y += Physics2D.gravity.y * Time.deltaTime; // 중력 적용
+        }
+        else if (velocity.y < 0)
+        {
+            velocity.y = 0; // 바닥에 닿으면 낙하 멈춤
+        }
+
+        // 애니메이션 처리
+        if (velocity.y < 0 && !isGrounded)
         {
             anim.SetBool("isJumping", false);
             anim.SetBool("isFalling", true);
         }
 
-        // 바닥에 닿았을 때 애니메이션 처리
-        if (isGrounded && rigid.linearVelocity.y <= 0)
+        if (isGrounded && velocity.y <= 0)
         {
             anim.SetBool("isJumping", false);
             anim.SetBool("isFalling", false);
         }
-        
-        
-    }
 
-
-
-
-
-    void FixedUpdate()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.linearVelocity = new Vector2(h * maxSpeed, rigid.linearVelocity.y);
+        // 실제 이동 적용
+        transform.Translate(new Vector3(h * maxSpeed * Time.deltaTime, velocity.y * Time.deltaTime, 0));
     }
 
     void FlipCollider(bool isFlipped)
     {
         boxCollider.offset = new Vector2(isFlipped ? -originalOffset.x : originalOffset.x, originalOffset.y);
+        groundCheck.localPosition = new Vector2(isFlipped? -originallocalPosition.x : originallocalPosition.x, originallocalPosition.y);
     }
 }
